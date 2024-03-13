48c7231ae455ce67261b56692c90c15b785cb4aa
Dhikash03 <dhikash9504@gmail.com>
2024-03-13 14:51:43 +0100

    Troisième commit

--- tableau3.php
+++ tableau3.php
@@ -1,7 +1,8 @@
+
 <?php
 $marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
 $i=0;
 foreach ($elem as $marque) {
-	echo "Vente de : " . $elem . "<br/>";
+echo "Vente de : " . $elem . "<br/>";
 }
 ?>
